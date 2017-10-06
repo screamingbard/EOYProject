b02fa0654580a8fa272d086eb4131c0bfddbd909
danjstuddert <danieljstuddert@gmail.com>
2017-10-06 22:45:03 +1100

    Vision cone editor visualization

---
+++ Assets/Editor/EditorVisualisations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VisionCone))]
+public class EditorVisualisations : Editor{
+
+
+    // in editor GUI for level design assistance
+    void OnSceneGUI(){
+        VisionCone m_vcVisualiser = (VisionCone)target;
+
+        //the colour of the in editor circle around the enemy with a vision cone
+        Handles.color = Color.yellow;
+        //draw a circle around the enemy indicating the radius of detection
+        Handles.DrawWireArc(m_vcVisualiser.transform.position, Vector3.forward, Vector3.up, 360, m_vcVisualiser.m_fRadius);
+
+        //Draws a cone withing the circle indication the area within which the enemy can detect the player
+        Vector3 viewAngleA = m_vcVisualiser.m_v3LookTarget(-m_vcVisualiser.m_fAngle / 2, false);
+        Vector3 viewAngleB = m_vcVisualiser.m_v3LookTarget(m_vcVisualiser.m_fAngle / 2, false);
+        Handles.DrawLine(m_vcVisualiser.transform.position, m_vcVisualiser.transform.position + viewAngleA * m_vcVisualiser.m_fRadius);
+        Handles.DrawLine(m_vcVisualiser.transform.position, m_vcVisualiser.transform.position + viewAngleB * m_vcVisualiser.m_fRadius);
+
+        //set the colour of the lines between visible player and enemy to red
+        Handles.color = Color.red;
+        //draw a line to the player from an enemy if that enemy can see the player
+        if (m_vcVisualiser.m_bPlayerVisible)
+        {
+            Handles.DrawLine(m_vcVisualiser.transform.position, m_vcVisualiser.m_tfPlayer.position);
+        }
+    }
+
+}
