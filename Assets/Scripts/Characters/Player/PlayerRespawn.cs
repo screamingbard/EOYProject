11873f8d8b3ae1396f0fe7594b26d8e388b34887
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-10 14:40:35 +1100

    Made the respawn function public and usable

--- Assets/Scripts/Characters/Player/PlayerRespawn.cs
+++ Assets/Scripts/Characters/Player/PlayerRespawn.cs
@@ -8,13 +8,26 @@ public class PlayerRespawn : MonoBehaviour {
     public string m_stVisionConeTag;
     //Death trap tag
     public string m_stDeathTrapTag;
+    //New respawn point tag
+    public string m_stNewRespawn;
     //The respawning point of the player
     public Transform m_tfRespawnPoint;
     //The player
     public GameObject m_goPlayer;
+    //The grapple
+    public GameObject m_goGrappleRefrence;
 
-    void Respawn(){
+    public void Respawn(){
         //Play death animation
+
+        //Set the players velocity to zero
+        m_goPlayer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        //If it is connected to terain or in motion
+        if (m_goGrappleRefrence != null){
+            //Remove the grapple
+            Destroy(m_goGrappleRefrence);
+        }
 
         //Set the players position to the respawn points
         m_goPlayer.transform.SetPositionAndRotation(m_tfRespawnPoint.position, m_tfRespawnPoint.rotation);
@@ -24,18 +37,24 @@ public class PlayerRespawn : MonoBehaviour {
     
     void OnCollisionEnter2D(Collision2D a_col2DCollider){
         //If the player enters the collider of an object DeathTrap
-        if (a_col2DCollider.gameObject.tag == m_stDeathTrapTag)
-        {
+        if (a_col2DCollider.gameObject.tag == m_stDeathTrapTag){
             //Call the respawn funtion
             Respawn();
         }
         //Or if the player enters the collider of an enemy vision cone
-        else if (a_col2DCollider.gameObject.tag == m_stVisionConeTag)
-        {
+        else if (a_col2DCollider.gameObject.tag == m_stVisionConeTag){
             //Play the killed by corvus animation
 
             //Call the respawn function
             Respawn();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D a_trTrigger2D){
+        //If the player enters the trigger zone of a new respawn point
+        if (a_trTrigger2D.gameObject.tag == m_stNewRespawn)
+        {
+            //set the new respawn point
+            m_tfRespawnPoint = a_trTrigger2D.gameObject.transform;
         }
     }
 }
