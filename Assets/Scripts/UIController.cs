1132cd424b7dfffa6bb757ac5a965187b960ae16
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-16 17:32:14 +1100

    UIController and updated Scene loaderer

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    public int m_iSceneIndex;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(m_iSceneIndex);
+    }
+}
