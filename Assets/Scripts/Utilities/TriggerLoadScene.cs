f151019f364a9990b968e3dd3a2b79680e8d3e81
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-16 17:18:29 +1100

    I will not forget these messages

---
+++ Assets/Scripts/Utilities/TriggerLoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TriggerLoadScene : MonoBehaviour {
+
+    public int m_iSceneIndex;
+
+    void OnTriggerEnter2D()
+    {
+        SceneManager.LoadScene(m_iSceneIndex);
+    }
+}
