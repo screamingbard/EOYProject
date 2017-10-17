e74743c798b5e378dc7b5b2e3a137ded7573a01d
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-17 15:05:28 +1100

    Commented code Head Movement and Vision Cone

--- Assets/Scripts/Characters/Corvus/HeadMovement.cs
+++ Assets/Scripts/Characters/Corvus/HeadMovement.cs
@@ -4,49 +4,58 @@ using UnityEngine;
 
 public class HeadMovement : MonoBehaviour
 {
-    //The location of the first look position target
+    //The location of the first look target
     public Transform m_tfInitialRotation;
 
-    //
+    //The location of the second look target
     public Transform m_tfSecondRotation;
 
-    //
+    //The speed of rotation
     [Range (0.01f, 60)]
     public float m_fRotationSpeed;
 
-    //
+    //The length of time inbetween the start of rotations before the direction is switched
     public float m_fTimeBetweenRotations;
     
     //The rotation the head is currently turning to
     Transform m_tfDesiredRotation;
     
-    //
+    //The counter for the time between look locations, based off the look time variable
     float m_fTimerBetweenRotations = 0;
     
-    // Use this for initialization
     void Start()
     {
+        //Initialise variables
+        //Set the Timer to the time to start the loop
         m_fTimerBetweenRotations = m_fTimeBetweenRotations;
+
+        //Set the desired rotation of the vision cone to the second set rotation
         m_tfDesiredRotation = m_tfSecondRotation;
     }
 
     void Update()
     {
-        Vector3 vectorToTarget = m_tfDesiredRotation.position - transform.position;
+        //Create a vector to a target
+        Vector3 m_v3VectorToTarget = m_tfDesiredRotation.position - transform.position;
         //If the cooldown of the rotation has ended
         if (m_fTimerBetweenRotations >= m_fTimeBetweenRotations)
         {
             //If the current rotation is the initial rotation
-            if (vectorToTarget == m_tfInitialRotation.position - transform.position)
+            if (m_v3VectorToTarget == m_tfInitialRotation.position - transform.position)
                 {
+                //Set the desired rotation to the secondary rotation
                     m_tfDesiredRotation = m_tfSecondRotation;
+                //Set the timer to the begining
                     m_fTimerBetweenRotations = 0;
                 }
                 //Otherwise if the current rotation is the second rotation
-            else if (vectorToTarget == m_tfSecondRotation.position - transform.position)
-                {
-                    m_tfDesiredRotation = m_tfInitialRotation;
-                    m_fTimerBetweenRotations = 0;
+            else if (m_v3VectorToTarget == m_tfSecondRotation.position - transform.position)
+            {
+                //Set the desired rotation to the initial rotation
+                m_tfDesiredRotation = m_tfInitialRotation;
+
+                //Set the timer to the begining
+                m_fTimerBetweenRotations = 0;
                 }
         }
         //Otherwise
@@ -55,9 +64,14 @@ public class HeadMovement : MonoBehaviour
             //Count up the cooldown timer
             m_fTimerBetweenRotations += Time.deltaTime;
         }
-        float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) - 90;
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * m_fRotationSpeed);
+        //The angle used in determining the quatonion used for the rotation
+        float m_fAngle = (Mathf.Atan2(m_v3VectorToTarget.y, m_v3VectorToTarget.x) * Mathf.Rad2Deg) - 90;
+
+        //The quatonion used for the rotation
+        Quaternion m_qAngle = Quaternion.AngleAxis(m_fAngle, Vector3.forward);
+
+        //The rotation of the vision cone is modified by the slerp call based on the qutonion previously created
+        transform.rotation = Quaternion.Slerp(transform.rotation, m_qAngle, Time.deltaTime * m_fRotationSpeed);
     }
 
 }
--- Assets/Scripts/Characters/Corvus/VisionCone.cs
+++ Assets/Scripts/Characters/Corvus/VisionCone.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class VisionCone : MonoBehaviour {
 
     //Length of the vision of an enemy
-    [Range(0, 50)]     public float m_fRadius;
+    [Range(0, 50)]
+    public float m_fRadius;
 
-    //width of the enemy vision
-    [Range(0, 360)]    public float m_fAngle = 27;
+    //Width of the enemy vision
+    [Range(0, 360)]
+    public float m_fAngle = 27;
 
-    //layer mask specific to the player
+    //Layer mask specific to the player
     public LayerMask m_lmPlayerMask;
 
-    //layer mask indicating obsticles to block the vision of an enemy
+    //Layer mask indicating obsticles to block the vision of an enemy
     public LayerMask m_lmObstacleMask;
 
     //Number of rays cast in the vision cone per a thing
@@ -27,13 +29,13 @@ public class VisionCone : MonoBehaviour {
     //Time the player has before they're killed within a vision cone
     float m_fUseDeathTimer;
 
-    //the transform of the player
+    //The transform of the player
     public Transform m_tfPlayer;
-    
-    //
+
+    //A mesh filter variable used for the vision cone ingame visualisation
     public MeshFilter m_mfViewMeshFilter;
 
-    //
+    //A mesh variable used for the vision cone ingame visualisation
     [HideInInspector]
     public Mesh m_mViewMesh;                   
 
@@ -41,73 +43,79 @@ public class VisionCone : MonoBehaviour {
     {
         //Set the death timer of the vision cone
         m_fUseDeathTimer = m_fDeathTimer;
-        //
+
+        //Initialise the view mesh
         m_mViewMesh = new Mesh();
 
-        //
+        //Name the view mesh, view mesh
         m_mViewMesh.name = "View Mesh";
         
-        //
+        //Set the mesh of the mesh filter to the view mesh
         m_mfViewMeshFilter.mesh = m_mViewMesh;
     }
 
     void LateUpdate()
     {
-        //
+        //Call the find player method
         FindPlayer();  
        
-        //
+        //Call the draw vision cone method
         DrawVisionCone();
     }
 
     void FindPlayer()
-    //Function to find the player within the vision of an enemy
+    //Method to find the player within the vision of an enemy
     {
 
-        //collider to check if the player is within the radius of an enemies vision
+        //Collider to check if the player is within the radius of an enemies vision
         bool m_bPlayerWithinView = Physics2D.OverlapCircle(transform.position, m_fRadius, m_lmPlayerMask);
 
-        //get the players tranform if the player is within the view radius of an enemy
+        //Get the players tranform if the player is within the view radius of an enemy
         if (m_bPlayerWithinView == true)
         {
-            //get the direction of the vector between the player and enemy
+            //Get the direction of the vector between the player and enemy
             Vector3 m_v3DirectionToPlayer = (m_tfPlayer.position - transform.position).normalized;
 
-            //get the angle bewtween the forward of the vision cone and the vector to the player
+            //Get the angle bewtween the forward of the vision cone and the vector to the player
             float m_fAngleBetween = (Vector3.Angle(m_v3DirectionToPlayer, transform.up));
 
-            //check for the player being within the cone of vision rather than just the radius
+            //Check for the player being within the cone of vision rather than just the radius
             if (m_fAngleBetween <= (m_fAngle / 2))
             {
-                //get the distance between the enemy and the player
+                //Get the distance between the enemy and the player
                 float m_fDistanceToPlayer = Vector3.Distance(transform.position, m_tfPlayer.position);
                 
-               //check for whether an obstacle is obstructing the enemies view of the player
+               //Check for whether an obstacle is obstructing the enemies view of the player
                if (!Physics2D.Raycast(transform.position, m_v3DirectionToPlayer, m_fDistanceToPlayer, m_lmObstacleMask))
                {
                    if (m_fUseDeathTimer <= 0)
                    {
-                       //if the enemy has direct line of sight to the player kill the player
+                       //If the enemy has direct line of sight to the player kill the player
                        m_tfPlayer.GetComponent<PlayerRespawn>().Respawn();
+                        //Reset the death timer
                        m_fUseDeathTimer = m_fDeathTimer;
                    }
                    else
-                   {
-                       m_fUseDeathTimer -= Time.deltaTime;
+                    {
+                        //Reset the death timer
+                        m_fUseDeathTimer -= Time.deltaTime;
                    }
                 }
                 else
                 {
+                    //Reset the death timer
                     m_fUseDeathTimer = m_fDeathTimer;
                 }
             }
             else
             {
+                //Reset the death timer
                 m_fUseDeathTimer = m_fDeathTimer;
             }
         }
         else
         {
+            //Reset the death timer
             m_fUseDeathTimer = m_fDeathTimer;
         }
     }
@@ -115,96 +123,120 @@ public class VisionCone : MonoBehaviour {
     void DrawVisionCone()
     //Draws the cone of vision in game
     {
-        //number of Raycast2Ds based on the angle of the vision cone and the mesh resolution
+        //Number of Raycast2Ds based on the angle of the vision cone and the mesh resolution
         int m_iStepCount = Mathf.RoundToInt(m_fAngle * m_fMeshResolution); 
-        //angle inbetween each Raycast2D
+        //Angle inbetween each Raycast2D
         float m_fStepAngleSize = m_fAngle / m_iStepCount;
-        //list of vectors to direct each Raycast2D
+        //List of vectors to direct each Raycast2D
         List<Vector3> m_lv3ViewPoints = new List<Vector3>();
-        //looping through the number of steps or Raycast2Ds
+        //Looping through the number of steps or Raycast2Ds
         for (int i = 0; i <= m_iStepCount; i++){
-            //angle used within this loop
+            //Angle used within this loop
             float m_fThisAngle = (transform.eulerAngles.z * -1) - m_fAngle/2 + m_fStepAngleSize * i;
-            Debug.DrawLine(transform.position, transform.position + m_v3LookTarget(m_fThisAngle, true) * m_fRadius, Color.red);
-            //use the ViewCast function to return a ViewCastInfo struct which contains information about an individual Raycast2D
+            //Use the ViewCast method to return a ViewCastInfo struct which contains information about an individual Raycast2D
             ViewCastInfo newViewCast = ViewCast(m_fThisAngle);
-            m_lv3ViewPoints.Add(newViewCast.point);
+            //Add a raycast to the list
+            m_lv3ViewPoints.Add(newViewCast.m_v3Point);
         }
+        //The number of vertecies
         int m_iVertexCount = m_lv3ViewPoints.Count + 1;
+
+        //An array of Verticies
         Vector3[] m_v3Vertices = new Vector3[m_iVertexCount];
+
+        //An array of triangles
         int[] m_iTriangles = new int[(m_iVertexCount - 2) * 3];
 
+        //Set the zeroeth vertex in the array to (0, 0, 0)
         m_v3Vertices[0] = Vector3.zero;
         for (int i = 0; i < m_iVertexCount - 1; i++)
+        //Go through the verticies array and set them
         {
             m_v3Vertices[i + 1] = transform.InverseTransformPoint(m_lv3ViewPoints[i]);
             
+            //If the iteration count is less than the vertex count minus two
             if (i < m_iVertexCount - 2)
             {
+                //Set the triangles to their relavent numbers
                 m_iTriangles[i * 3] = 0;
                 m_iTriangles[i * 3 + 1] = i + 1;
                 m_iTriangles[i * 3 + 2] = i + 2;
             }
         }
 
+        //Clear the view mesh
         m_mViewMesh.Clear();
+
+        //Set the vertecies of the view mesh
         m_mViewMesh.vertices = m_v3Vertices;
+
+        //Set the triangles of the view mesh
         m_mViewMesh.triangles = m_iTriangles;
+
+        //Recalculate the normals of the view mesh
         m_mViewMesh.RecalculateNormals();
     }
 
-    ViewCastInfo ViewCast(float globalAngle)
+    ViewCastInfo ViewCast(float a_fGlobalAngle)
     {
-        Vector3 dir = m_v3LookTarget(globalAngle, true);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, m_fRadius, m_lmObstacleMask );
-        if (hit){
-            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
-        } else {
-            return new ViewCastInfo(false, transform.position + dir * m_fRadius, m_fRadius, globalAngle);
+        //The look direction of the vision cone relative to global
+        Vector3 m_v3Direction = m_v3LookTarget(a_fGlobalAngle, true);
+        //A test for if the rays hit something
+        RaycastHit2D m_v3Hit = Physics2D.Raycast(transform.position, m_v3Direction, m_fRadius, m_lmObstacleMask );
+        //If the rays did hit
+        if (m_v3Hit)
+        {
+            //Returns a new view cast info struct with relavent information
+            return new ViewCastInfo(true, m_v3Hit.point, m_v3Hit.distance, a_fGlobalAngle);
+        }
+        //Otherwise
+        else
+        {
+            //Returns a new view cast info struct with different relavent information
+            return new ViewCastInfo(false, transform.position + m_v3Direction * m_fRadius, m_fRadius, a_fGlobalAngle);
         }
     }
-    
     public Vector3 m_v3LookTarget(float a_fAngleInDegrees, bool a_bAngleIsGlobal){
         //Moves the cone around the circle when the object is rotated
         if (!a_bAngleIsGlobal)
         {
-            //
+            //Invert the angle so that it follows the correctorientation
             a_fAngleInDegrees -= transform.eulerAngles.z;
         }
 
-        //
+        //Return the angle in degrees the diference is between the look target and the forward
         return new Vector3(Mathf.Sin(a_fAngleInDegrees * Mathf.Deg2Rad), Mathf.Cos(a_fAngleInDegrees * Mathf.Deg2Rad), 0);
     }
 
     public struct ViewCastInfo
     {
-        //
-        public bool hit;
+        //Boolean variable to check if the raycast has hit anything
+        public bool m_bHit;
 
-        //
-        public Vector3 point;
+        //Vector 3 variable to get the
+        public Vector3 m_v3Point;
 
-        //
-        public float dst;
+        //Float variable to get the distance the ray cast goes before it hits someting, will be max range if it doesn't hit
+        public float m_fDistance;
 
-        //
-        public float angle;
+        //Float variable to get the angle between the forward and the target
+        public float m_fVCIAngle;
 
         
-        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle)
-       //
+        public ViewCastInfo(bool a_bHit, Vector3 a_v3Point, float a_fDistance, float a_fAngle)
+       //Sets all the variables within the view cast info struct
         {
-            //
-            hit = _hit;
+            //Sets the hit boolean
+            m_bHit = a_bHit;
 
-            //
-            point = _point;
+            //Sets the point variable
+            m_v3Point = a_v3Point;
 
-            //
-            dst = _dst;
+            //Sets the distance variable
+            m_fDistance = a_fDistance;
 
-            //
-            angle = _angle;
+            //Sets the angle variable
+            m_fVCIAngle = a_fAngle;
         }
     }
     
