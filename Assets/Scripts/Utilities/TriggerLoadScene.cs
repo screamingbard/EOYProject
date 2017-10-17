f53162102caaaab170e2321341fd16293e2d0a92
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-17 15:06:18 +1100

    Commented Code Trigger Load Scene

--- Assets/Scripts/Utilities/TriggerLoadScene.cs
+++ Assets/Scripts/Utilities/TriggerLoadScene.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class TriggerLoadScene : MonoBehaviour {
 
+    //The variable controlling which scene is loaded in the scene load method
     public int m_iSceneIndex;
 
     void OnTriggerEnter2D()
+    //When the player passes through the trigger will load a specified scene
     {
         SceneManager.LoadScene(m_iSceneIndex);
     }
