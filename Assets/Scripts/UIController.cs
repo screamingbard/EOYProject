12499732e48da56c9323ced849724181b50a3d30
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-18 11:53:35 +1100

    Added button controller selecterer

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UIController : MonoBehaviour {
 
     //The variable controlling which scene is loaded in the scene load method
     public int m_iSceneIndex = 0;
 
+    //
+    public Button restartButton;
+
+    //
+    public EventSystem EventSysRef;
+
+    void Awake()
+    {
+        EventSysRef.SetSelectedGameObject(restartButton.gameObject);
+    }
     public void Quit()
     //On call will close the game
     {
