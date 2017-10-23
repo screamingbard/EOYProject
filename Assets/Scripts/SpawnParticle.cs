fc100bba60ce8d575b2ec955dcf9cca19acdc001
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-23 16:46:45 +1100

    Spawn Particle on trigger with controllable thingies

---
+++ Assets/Scripts/SpawnParticle.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticle : MonoBehaviour {
+
+    //
+    public string m_sPlayerTag;
+    //
+    public ParticleSystem m_psParticleSystem;
+
+    //
+    public bool m_bPlayMoreThanOnce;
+    
+    //
+    public float m_fTimeBetweenSpawns;
+
+    //
+    float m_fTimerBetweenSpawns;
+
+    //
+    bool m_bPlayMoreThanOncePrivate = true;
+
+    void start()
+    {
+        //
+        m_fTimerBetweenSpawns = m_fTimeBetweenSpawns;
+    }
+
+    void Update()
+    {
+
+        if (m_bPlayMoreThanOncePrivate)
+        {
+            if (m_fTimerBetweenSpawns >= m_fTimeBetweenSpawns)
+            {
+                m_fTimerBetweenSpawns = 0;
+            }
+            else
+            {
+                m_fTimerBetweenSpawns += Time.deltaTime;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider2D a_colCollider)
+    {
+        if (a_colCollider.gameObject.tag == m_sPlayerTag)
+        {
+            //If true will play the animation
+            if (m_bPlayMoreThanOncePrivate)
+            {
+                if (m_fTimerBetweenSpawns >= m_fTimeBetweenSpawns)
+                {
+                    m_fTimerBetweenSpawns = 0;
+                    m_psParticleSystem.Play();
+                    m_bPlayMoreThanOncePrivate = m_bPlayMoreThanOnce;
+                }
+            }
+        }
+    }
+
+}
