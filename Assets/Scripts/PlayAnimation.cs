fb36bd1e119fd8035a84d709aff693107dc204ef
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-10-23 17:13:52 +1100

    On trigger plays animations (should)

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour {
+
+    //
+    public string m_sPlayerTag;
+
+    //
+    public Animation m_animAnimation;
+
+    //
+    public bool m_bPlayMoreThanOnce;
+
+    //
+    public float m_fTimeBetweenAnimations;
+
+    //
+    float m_fTimerBetweenAnimations;
+
+    //
+    bool m_bPlayMoreThanOncePrivate = true;
+
+    void start()
+    {
+        //
+        m_fTimerBetweenAnimations = m_fTimeBetweenAnimations;
+    }
+
+    void Update()
+    {
+
+        if (m_bPlayMoreThanOncePrivate)
+        {
+            if (m_fTimerBetweenAnimations >= m_fTimeBetweenAnimations)
+            {
+                m_fTimerBetweenAnimations = 0;
+            }
+            else
+            {
+                m_fTimerBetweenAnimations += Time.deltaTime;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider2D a_colCollider)
+    {
+        if (a_colCollider.gameObject.tag == m_sPlayerTag)
+        {
+            //If true will play the animation
+            if (m_bPlayMoreThanOncePrivate)
+            {
+                if (m_fTimerBetweenAnimations >= m_fTimeBetweenAnimations)
+                {
+                    m_fTimerBetweenAnimations = 0;
+                    m_animAnimation.Play();
+                    m_bPlayMoreThanOncePrivate = m_bPlayMoreThanOnce;
+                }
+            }
+        }
+    }
+
+}
