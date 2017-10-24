3a2ce1c409a52cfce295cc8882dc245bdf03aadd
Hypro123 <eddie.ultra@gmail.com>
2017-10-24 16:04:33 +1100

    will disabled particles when player triggers the points

---
+++ Assets/Scripts/Utilities/DisableParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableParticleSystem : MonoBehaviour {
+
+    public ParticleSystem psParticleReference;
+
+    void Awake()
+    {
+        psParticleReference = GetComponent<ParticleSystem>();
+        ParticleSystem.EmissionModule isEmmiting = psParticleReference.emission;
+        isEmmiting.enabled = true;
+    }
+
+    void OntriggerEnter2D(Collision2D collision2d)
+    {
+        if(collision2d.gameObject.tag == "Player")
+        {
+            ParticleSystem.EmissionModule isEmmiting = psParticleReference.emission;
+            isEmmiting.enabled = false;
+        }
+    }
+}
