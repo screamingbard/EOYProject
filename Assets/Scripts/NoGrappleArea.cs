cc70f8fd655112a9e2498397d4bf8316699c58f8
Hypro123 <eddie.ultra@gmail.com>
2017-10-25 11:21:46 +1100

    NoGrappleAreaScript

---
+++ Assets/Scripts/NoGrappleArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoGrappleArea : MonoBehaviour {
+
+    public GameObject goplayer;
+
+    void Awake()
+    {
+        goplayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2d)
+    {
+        if(collision2d.gameObject.tag == "Grapple"){
+            goplayer.GetComponentInChildren<ShootOBJ>().StopShoot();
+        }
+    }
+
+}
