7fdd38f8f4657825660e2fc346aa7914cd245a00
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-11-06 16:15:58 +1100

    contains a public menu music variable and 2 lists of public audio clips one of which is background music and the other is of ambient sounds, the only music that plays is the menu music as the script starts

---
+++ Assets/Scripts/Utilities/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    //
+    AudioSource m_asAudioSource;
+
+    //
+    public GameData m_gdGameData;
+
+    //
+    public AudioClip m_acMenuMusic;
+
+    //
+    public List<AudioClip> m_lacBGM;
+
+    //
+    public List<AudioClip> m_lacAmbiantSounds;
+
+	// Use this for initialization
+	void Start () {
+        m_asAudioSource.PlayOneShot(m_acMenuMusic, 0.5f/*m_gdGameData.m_setSettigs.m_fMusicVolume * m_gdGameData.m_setSettigs.m_fMasterVolume*/);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
