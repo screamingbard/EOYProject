81c2015f29821eaeea7617df5086ce957387dced
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-11-08 12:26:42 +1100

    Public and renamed the ambient audio source

--- Assets/Scripts/Utilities/SoundManager.cs
+++ Assets/Scripts/Utilities/SoundManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour {
 
     //
-    AudioSource m_asAudioSource;
+    public AudioSource m_asAmbientAudioSource;
 
     //
     public GameData m_gdGameData;
@@ -22,15 +22,15 @@ public class SoundManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        m_asAudioSource.PlayOneShot(m_acMenuMusic, 0.5f/*m_gdGameData.m_setSettigs.m_fMusicVolume * m_gdGameData.m_setSettigs.m_fMasterVolume*/);
+        m_asAmbientAudioSource.PlayOneShot(m_acMenuMusic, 0.5f/*m_gdGameData.m_setSettigs.m_fMusicVolume * m_gdGameData.m_setSettigs.m_fMasterVolume*/);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!m_asAudioSource.isPlaying)
+        if (!m_asAmbientAudioSource.isPlaying)
         {
-            m_asAudioSource.clip = m_lacAmbientSounds[Random.Range(0, m_lacAmbientSounds.Count)];
-            m_asAudioSource.Play();
+            m_asAmbientAudioSource.clip = m_lacAmbientSounds[Random.Range(0, m_lacAmbientSounds.Count)];
+            m_asAmbientAudioSource.Play();
         }
 	}
 }
