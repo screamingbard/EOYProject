3e80914c293b8f1eaa895f1991e2417cc4894f7d
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-11-08 17:17:07 +1100

    Sounds 'n' things

--- Assets/Scripts/Utilities/SoundManager.cs
+++ Assets/Scripts/Utilities/SoundManager.cs
@@ -8,6 +8,9 @@ public class SoundManager : MonoBehaviour {
     public AudioSource m_asAmbientAudioSource;
 
     //
+    public AudioSource m_asMusicAudioSource;
+
+    //
     public GameData m_gdGameData;
 
     //
@@ -19,17 +22,17 @@ public class SoundManager : MonoBehaviour {
     //
     public List<AudioClip> m_lacAmbientSounds;
 
-
-	// Use this for initialization
+    
 	void Start () {
         m_asAmbientAudioSource.PlayOneShot(m_acMenuMusic, 0.5f/*m_gdGameData.m_setSettigs.m_fMusicVolume * m_gdGameData.m_setSettigs.m_fMasterVolume*/);
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
         if (!m_asAmbientAudioSource.isPlaying)
         {
             m_asAmbientAudioSource.clip = m_lacAmbientSounds[Random.Range(0, m_lacAmbientSounds.Count)];
+            m_asAmbientAudioSource.volume = 0.5f /*m_gdGameData.m_setSettings.m_fMusicVolume * m_gdGameData.m_setSettings.m_fMasterVolume*/;
             m_asAmbientAudioSource.Play();
         }
 	}
