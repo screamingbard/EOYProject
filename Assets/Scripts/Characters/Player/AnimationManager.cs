74a14b5c0beaaaf0f9a0a980a1f17350ce9ea850
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-11-13 12:17:09 +1100

    Might now know when it's grounded

--- Assets/Scripts/Characters/Player/AnimationManager.cs
+++ Assets/Scripts/Characters/Player/AnimationManager.cs
@@ -5,41 +5,49 @@ using XboxCtrlrInput;
 
 public class AnimationManager : MonoBehaviour {
 
-    //
+    //The animator
     public Animator m_animatorAnimator;
 
-    //
+    //The string for use to connect and control mechanim specifically for the walking animation
     public string m_stIsWalking;
 
-    //
+    //The string for use to connect and control mechanim specifically for the jumping animation
     public string m_stIsJumping;
 
-    //
+    //The string for use to connect and control mechanim specifically for the swinging animation
     public string m_stIsSwinging;
 
     //
     public bool m_bIsGrounded;
 
-    //
+    //Check for if the grapple is out and about
     public bool m_bGrappleIsOut;
 
-    //
+    //Reference to the player
     public GameObject m_goPlayer;
 
-    //
-    public PlayerController m_plycntPlayerContorller;
-
-    void Start()
-    {
-    }
-    // Update is called once per frame
+    //Reference to the player controller
+    public Controller2D m_pcPlayerContorller;
+    
     void Update () {
-        if (!m_plycntPlayerContorller.IsGrounded)
+        if (!m_pcPlayerContorller.collisions.below)
         {
             m_animatorAnimator.SetBool(m_stIsWalking, false);
+            if (m_bGrappleIsOut)
+            {
+                m_animatorAnimator.SetBool(m_stIsSwinging, true);
+                m_animatorAnimator.SetBool(m_stIsJumping, false);
+            }
+            else
+            {
+                m_animatorAnimator.SetBool(m_stIsJumping, true);
+                m_animatorAnimator.SetBool(m_stIsSwinging, false);
+            }
         }
         else
         {
+            m_animatorAnimator.SetBool(m_stIsJumping, false);
+            m_animatorAnimator.SetBool(m_stIsSwinging, false);
             if (XCI.GetAxisRaw(XboxAxis.LeftStickX) != 0)
             {
                 m_animatorAnimator.SetBool(m_stIsWalking, true);
