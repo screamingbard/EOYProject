284944531e626d8b25a086349c572ac7e3efbb48
Hypro123 <eddie.ultra@gmail.com>
2017-11-21 10:24:43 +1100

    Speeds randomly change based on a range

--- Assets/Scripts/Look/BirdbackgroundSummon.cs
+++ Assets/Scripts/Look/BirdbackgroundSummon.cs
@@ -17,11 +17,6 @@ public class BirdbackgroundSummon : MonoBehaviour {
     //the many scrips from the plaeyr detect script.
     //----------------------------------------------------------------------------------
     public string playerTag;
-    
-    //------------------------------------------------------------------------------------------------------
-    //This variabel controls the speed in which the bird moves in.
-    //------------------------------------------------------------------------------------------------------
-    public float birdSpeedVar = 20;
 
     //------------------------------------------------------------------------------------------------------
     //This Checks which Spawn point is currently Selected, 
@@ -42,10 +37,10 @@ public class BirdbackgroundSummon : MonoBehaviour {
     //------------------------------------------------------------------------------------------------------
     //This holds the current selected bird.
     //------------------------------------------------------------------------------------------------------
-    private List<GameObject> birdsStore = new List<GameObject>();
+    private List<birdus> birdsStore = new List<birdus>();
 
     //------------------------------------------------------------------------
-    //This offset variable makes the next bird spaw just below the last bird.
+    //This offset variable makes the next bird spawn just below the last bird.
     //------------------------------------------------------------------------
     public float Offset;
 
@@ -58,6 +53,18 @@ public class BirdbackgroundSummon : MonoBehaviour {
     //This is the starting point that the bird will spawn at then teleport back to after reaching the end.
     //------------------------------------------------------------------------------------------------------
     public GameObject Start;
+
+    //------------------------------------------------------------------------------------------------------
+    //This is the minimum speed of the roaming birds.
+    //------------------------------------------------------------------------------------------------------
+    [Range(0, float.MaxValue)]
+    public float minSpeed = 15;
+
+    //------------------------------------------------------------------------------------------------------
+    //This is the maximum speed of the roaming birds.
+    //------------------------------------------------------------------------------------------------------
+    [Range(0, float.MaxValue)]
+    public float maxSpeed = 16;
 
     //------------------------------------------------------------------------------------------------------
     //This end point limits how far the bird is able to fly,
@@ -84,7 +91,7 @@ public class BirdbackgroundSummon : MonoBehaviour {
         //Makes the birds keep flying and resetting when required
         CycleBird();
     }
-    
+
     //------------------------------------------------------------------------------------------------------
     //This method makes the bird actually move,
     //This cycles through all of the objects stored in the birdStore list and makes sure that they move
@@ -93,18 +100,18 @@ public class BirdbackgroundSummon : MonoBehaviour {
     void CycleBird()
     {
             //The loop that cycles through all of the variables in birdStore
-            foreach (GameObject go in birdsStore)
+            foreach (birdus go in birdsStore)
             {
-                //makes the bird move based on a speed and deltaTime
-                go.transform.position += new Vector3(birdSpeedVar * Time.deltaTime, 0, 0);
+            //makes the bird move based on a speed and deltaTime
+                go.Chicken.transform.position += new Vector3(go.speed * Time.deltaTime, 0, 0);
 
                 //This if statement makes sure that if the bird is moving towards the right it will keep looping in order to move to the right.
-                if (go.transform.position.x > End.transform.position.x)
-                    go.transform.position = new Vector3(Start.transform.position.x, go.transform.position.y, go.transform.position.z);
+                if (go.Chicken.transform.position.x > End.transform.position.x)
+                    go.Chicken.transform.position = new Vector3(Start.transform.position.x, go.Chicken.transform.position.y, go.Chicken.transform.position.z);
 
                 //This if statement makes sure that if the bird is moving towards the left it will keep looping in order to move to the left.
-                if (go.transform.position.x < Start.transform.position.x)
-                    go.transform.position = new Vector3(End.transform.position.x, go.transform.position.y, go.transform.position.z);
+                if (go.Chicken.transform.position.x < Start.transform.position.x)
+                    go.Chicken.transform.position = new Vector3(End.transform.position.x, go.Chicken.transform.position.y, go.Chicken.transform.position.z);
             }
     }
 
@@ -118,14 +125,17 @@ public class BirdbackgroundSummon : MonoBehaviour {
         if (birdsStore.Count > 0)
         {
             //creates a temp gameobject to store the bird before putting it into the list
-            GameObject temp;
+            birdus temp;
 
             //offsets the birds location by a number to make sure the birds dont spawn on eachother
             Offset = StoreOffset - (CurrentSelectedSpawnCount * objheight);
 
             //puts the instantiated bird into the variable and changes its variables for usage
-            temp = Instantiate(birdPrefab, Start.transform.position - new Vector3(0, Start.transform.position.y - Offset, 0), Quaternion.identity);
-            
+            temp.Chicken = Instantiate(birdPrefab, Start.transform.position - new Vector3(0, Start.transform.position.y - Offset, 0), Quaternion.identity);
+
+            //Adds the temporary object to the birdStore list
+            temp.speed = RandomSpeedGen();
+
             //adds the bird object to a list of bird gameobjects
             birdsStore.Add(temp);
             
@@ -137,16 +147,53 @@ public class BirdbackgroundSummon : MonoBehaviour {
         if (birdsStore.Count <= 0)
         {
             //Temporary variable that stores which 
-            GameObject temp;
+            birdus temp;
 
             //temp variable that stores the instantiated object
-            temp = Instantiate(birdPrefab, Start.transform.position, Quaternion.identity);
+            temp.Chicken = Instantiate(birdPrefab, Start.transform.position, Quaternion.identity);
 
-            //Adds the temporary object to the birdStore list
+            //Sets the speed of the next created birds
+            temp.speed = RandomSpeedGen();
+
+            //Sets the speed of the first created bird
             birdsStore.Add(temp);
 
             //increments the last selected point
             CurrentSelectedSpawnCount++;
         }
     }
+
+    //--------------------------------------------------------------------------------------------------------------------
+    //This method controls the speed of the roaming birds by randomly generating a number between the maximum and minimum.
+    //Safety measures are put in.
+    //--------------------------------------------------------------------------------------------------------------------
+    float RandomSpeedGen()
+    {
+        //To make sure that the speed variable will not be overly broken
+        //This changes the minimum speed to be the maximum if the minimum variable is larger than the maximum
+        if (minSpeed > maxSpeed)
+        {
+            //temporarily stores the minimum speed for switching
+            float temp = minSpeed;
+        
+            //Switches the min and max to the desired values
+            minSpeed = maxSpeed;
+            maxSpeed = temp;
+        }
+        
+        //Randomly generates a number based on a range
+        float speed = Random.Range(minSpeed, maxSpeed);
+        
+        //returns the speed
+        return speed;
+    }
+}
+
+//------------------------------------------------------
+//holds the gameobject and speed of the created birds.
+//------------------------------------------------------
+struct birdus
+{
+    public GameObject Chicken;
+    public float speed;
 }
