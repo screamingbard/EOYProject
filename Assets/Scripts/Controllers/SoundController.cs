2af9cd62e5a4d33ee44e7b7dc94210d59606dca0
screamingbard <31602613+screamingbard@users.noreply.github.com>
2017-11-22 11:26:30 +1100

    Sounds 'n' things

--- Assets/Scripts/Controllers/SoundController.cs
+++ Assets/Scripts/Controllers/SoundController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour {
+public class SoundController : MonoBehaviour {
 
     //The audio scource for the ambient sounds
     public AudioSource m_asAmbientAudioSource;
