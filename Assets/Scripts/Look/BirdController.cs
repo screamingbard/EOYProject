11637b69bc7a42cc6a40b27a85e106ccf4836ab9
Hypro123 <eddie.ultra@gmail.com>
2017-11-29 09:00:28 +1100

    commenting

--- Assets/Scripts/Look/BirdController.cs
+++ Assets/Scripts/Look/BirdController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //+=+=+=+=+=+=+=+=+=+=+
-//Edward Ladyzhenskii
+//Author: Edward Ladyzhenskii
 //+=+=+=+=+=+=+=+=+=+=+
 
 //+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
